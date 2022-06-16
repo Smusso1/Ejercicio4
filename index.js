05a420c9d059e5c3c8b7ca1cbccb9e4c2ab6cc0b
Smusso1 <102436160+Smusso1@users.noreply.github.com>
2022-06-17 06:55:04 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+const rutas = require('./rutas1/productsRoutes')
+const middleware = (req, res, next) => {
+    console.log('Llego una request')
+    next()
+}
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/", express.static(__dirname + "/public"))
+app.use(express.static('public'))
+app.use(middleware)
+
+app.use('/api', rutas)
+
+app.use((error, req, res, next) => {
+    console.log(error)
+    res.sendStatus(500)
+})
+
+app.listen(8080, () => {
+    console.log('Serdvidor escuchando el puerto 8080')
+})
+
+
